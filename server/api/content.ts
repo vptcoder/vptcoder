43fadd219a372b212c64ea7f3034e41c5d0b06b1
vptcoder <vptcoder@outlook.com>
2022-12-11 14:46:11 +0800

    feat: api call

---
+++ server/api/content.ts
@@ -0,0 +1,7 @@
+// import {defineEventHandler} from 'h3';
+// export default defineEventHandler((event: any) => {
+//     console.log(event)
+//     return {
+//         api: 'content'
+//     }
+// })
