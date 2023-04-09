d4e365d1c45a65c8fd23604915fee8d56a7a3d4e
vptcoder <vptcoder@outlook.com>
2023-04-09 22:17:12 +0800

    cicd: resolve linter issue

--- pages/index.vue
+++ pages/index.vue
@@ -57,8 +57,8 @@ const cards = [
       >
         <TCardH
           v-for="(card, index) in cards"
-          :card="card"
           :key="card.id"
+          :card="card"
           :index="index"
         >
           <!-- eslint-disable vue/no-v-html -->
